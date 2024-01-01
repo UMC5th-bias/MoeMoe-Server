d6073235391cf3d6c1c24ec49b5e356215e5acc8
jorippppong <duelee75@gmail.com>
2024-01-01 12:17:42 +0900

    first commit

---
+++ src/main/java/com/favoriteplace/FavoritePlaceApplication.java
@@ -0,0 +1,13 @@
+package com.favoriteplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoritePlaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FavoritePlaceApplication.class, args);
+    }
+
+}
