d1da9ea5cb6bdc735a6ca5728ede8640b13ab86a
JungYoonShin <sju08227@naver.com>
2024-01-17 13:34:26 +0900

    .

--- src/main/java/com/favoriteplace/app/service/MemberService.java
+++ src/main/java/com/favoriteplace/app/service/MemberService.java
@@ -12,32 +12,5 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class MemberService {
-    private final MemberRepository memberRepository;
-    //private final JwtUtil jwtUtil;
 
-    @Transactional
-    public boolean isTokenExists(String accessToken) {
-        /*
-        [JWT]
-        String userId = jwtUtil.getUserIdFromToken(accessToken);
-        return memberRepository.existsById(Long.valueOf(userId));
-        */
-        //[임시] : accessToken 대신 memberId 사용
-        return memberRepository.existsById(Long.valueOf(accessToken));
-    }
-
-    @Transactional
-    public HomeResponseDto.UserInfo getUserInfo(String accessToken) {
-        /*
-        [JWT]
-        String userId = jwtUtil.getUserIdFromToken(accessToken);
-        Member member = memberRepository.findById(Long.valueOf(accessToken))
-                .orElseThrow(() -> new RestApiException(ErrorCode.USER_NOT_FOUND));
-        return HomeResponseDto.UserInfo.of(member);
-         */
-        //[임시] : accessToken 대신 memberId 사용
-        Member member = memberRepository.findById(Long.valueOf(accessToken))
-                .orElseThrow(() -> new RestApiException(ErrorCode.USER_NOT_FOUND));
-        return HomeResponseDto.UserInfo.of(member);
-    }
 }
