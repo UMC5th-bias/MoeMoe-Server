a595bb7fa7579fc9660b39a404573717777f052d
JungYoonShin <sju08227@naver.com>
2024-01-30 14:50:33 +0900

    [CHORE] start.sh 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -17,11 +17,10 @@ cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
 nohup sudo java -jar \
-        -Dspring.config.location=/home/ubuntu/app/application.yml \
-        -Dspring.config.additional-location=file:/home/ubuntu/app/studious-matrix-412103-04030f986b9b.json \
-        -Duser.timezone=Asia/Seoul \
-        -Dlogging.level.org.springframework=DEBUG \
-        $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+     -Dspring.config.location=/home/ubuntu/app/application.yml \
+     -Duser.timezone=Asia/Seoul \
+     -Dgcp.storage.credentials.location=env:GCP_STORAGE_CREDENTIALS \
+     $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
