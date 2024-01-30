6791871f24e9abe0071be84d28864e26a4a39c34
JungYoonShin <sju08227@naver.com>
2024-01-30 21:59:40 +0900

    [CHORE] start.sh 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,12 +10,14 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
+source ~/.bash_profile
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
+
 nohup sudo java -jar \
         -Dspring.config.location=/home/ubuntu/app/application.yml \
         -Duser.timezone=Asia/Seoul\
