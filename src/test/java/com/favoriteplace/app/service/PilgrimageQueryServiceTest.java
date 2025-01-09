ee889800a3a02934201cebe8868875f3f1a5fa9f
ppparkta <pppppark2@gmail.com>
2025-01-09 20:49:36 +0900

    [TEST/#169] 성지순례 애니별 카테고리 조회 성공 테스트 추가

--- src/test/java/com/favoriteplace/app/service/PilgrimageQueryServiceTest.java
+++ src/test/java/com/favoriteplace/app/service/PilgrimageQueryServiceTest.java
@@ -16,11 +16,12 @@ import com.favoriteplace.app.repository.RallyRepository;
 import com.favoriteplace.app.repository.VisitedPilgrimageRepository;
 import java.util.List;
 import org.assertj.core.api.Assertions;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Nested;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -53,28 +54,47 @@ class PilgrimageQueryServiceTest {
                 hashtagRepository, imageRepository);
     }
 
-    @Test
-    @DisplayName("성지순례 애니 별 카테고리 조회 성공")
-    void 성지순례_애니별_카테고리_조회() {
-        //given
-        Member member = Mockito.mock(Member.class);
-        Rally rallyOne = Mockito.mock(Rally.class);
-        Image image = Mockito.mock(Image.class);
-        Mockito.when(image.getUrl()).thenReturn("이미지");
-        Mockito.when(rallyOne.getId()).thenReturn(0L);
-        Mockito.when(rallyOne.getName()).thenReturn("최애의아이");
-        Mockito.when(rallyOne.getImage()).thenReturn(image);
-        Rally rallyTwo = Mockito.mock(Rally.class);
-        Mockito.when(rallyTwo.getId()).thenReturn(1L);
-        Mockito.when(rallyTwo.getName()).thenReturn("날씨의아이");
-        Mockito.when(rallyTwo.getImage()).thenReturn(image);
+    @Nested
+    @DisplayName("성지순례 애니 별 카테고리 조회")
+    class AnemeCategories {
 
-        //when
-        Mockito.when(rallyRepository.findAllOrderByCreatedAt()).thenReturn(List.of(rallyOne, rallyTwo));
-        List<PilgrimageCategoryAnimeDto> categoryAnime = pilgrimageService.getCategoryAnime(null);
+        @Test
+        @DisplayName("비로그인 성지순례 애니 별 카테고리 조회 성공")
+        void 비로그인_성지순례_애니별_카테고리_조회() {
+            //given
+            Image image = Mockito.mock(Image.class);
+            Mockito.when(image.getUrl()).thenReturn("이미지");
+            Rally rallyOne = getRally(0L, "최애의아이", image);
+            Rally rallyTwo = getRally(1L, "날씨의아이", image);
 
-        //then
-        Assertions.assertThat(categoryAnime.get(0).getId()).isEqualTo(0L);
-        Assertions.assertThat(categoryAnime.get(1).getId()).isEqualTo(1L);
+            //when
+            Mockito.when(rallyRepository.findAllOrderByCreatedAt()).thenReturn(List.of(rallyOne, rallyTwo));
+            List<PilgrimageCategoryAnimeDto> categoryAnime = pilgrimageService.getCategoryAnime(null);
+
+            //then
+            Assertions.assertThat(categoryAnime.get(0).getId()).isEqualTo(0L);
+            Assertions.assertThat(categoryAnime.get(1).getId()).isEqualTo(1L);
+        }
+
+        @Test
+        @DisplayName("로그인 성지순례 애니 별 카테고리 조회 성공")
+        void 로그인_성지순례_애니별_카테고리_조회() {
+            //given
+
+            //when
+
+            //then
+        }
+
+        @NotNull
+        private Rally getRally(Long id, String name, Image image) {
+            Rally rally = Mockito.mock(Rally.class);
+            Mockito.when(rally.getId()).thenReturn(id);
+            Mockito.when(rally.getName()).thenReturn(name);
+            Mockito.when(rally.getImage()).thenReturn(image);
+            return rally;
+        }
     }
+
+
 }
