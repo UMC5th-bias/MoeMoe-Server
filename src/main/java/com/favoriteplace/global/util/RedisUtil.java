d7978e61aa6839e22ca21b74405cee726994d709
정윤 <sju08227@naver.com>
2025-05-15 23:25:31 +0900

    [FIX] StringRedisTemplate에서 RedisTemplate<String, String>으로 변경

--- src/main/java/com/favoriteplace/global/util/RedisUtil.java
+++ src/main/java/com/favoriteplace/global/util/RedisUtil.java
@@ -2,6 +2,7 @@ package com.favoriteplace.global.util;
 
 import java.time.Duration;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
@@ -9,17 +10,17 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class RedisUtil {
-    private final StringRedisTemplate redisTemplate;//Redis에 접근하기 위한 Spring의 Redis 템플릿 클래스
+    private final RedisTemplate<String, String> redisTemplate;
 
-    public String getData(String key){//지정된 키(key)에 해당하는 데이터를 Redis에서 가져오는 메서드
+    public String getData(String key){
         ValueOperations<String,String> valueOperations = redisTemplate.opsForValue();
         return valueOperations.get(key);
     }
-    public void setData(String key, String value){//지정된 키(key)에 값을 저장하는 메서드
+    public void setData(String key, String value){
         ValueOperations<String,String> valueOperations = redisTemplate.opsForValue();
         valueOperations.set(key, value);
     }
-    public void setDataExpire(String key, String value, long duration){//지정된 키(key)에 값을 저장하고, 지정된 시간(duration) 후에 데이터가 만료되도록 설정하는 메서드
+    public void setDataExpire(String key, String value, long duration){
         ValueOperations<String,String> valueOperations = redisTemplate.opsForValue();
         Duration expireDuration = Duration.ofSeconds(duration);
         valueOperations.set(key, value, expireDuration);
