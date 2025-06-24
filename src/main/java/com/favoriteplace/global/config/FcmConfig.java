92a7c22a7d9ba5eb09cf6bfd5731b8a252107d09
정윤 <sju08227@naver.com>
2025-06-24 20:23:56 +0900

    [FIX] @ConditionalOnProperty 제거

--- src/main/java/com/favoriteplace/global/config/FcmConfig.java
+++ src/main/java/com/favoriteplace/global/config/FcmConfig.java
@@ -5,7 +5,6 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +12,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 @Configuration
-@ConditionalOnProperty(prefix = "fcm", name = "enabled", havingValue = "true")
 public class FcmConfig {
     @Value("${fcm.type}")
     private String type;
